830649041b4ff7f2ab3e537b256ea6faf4030ff4
iqbalwalii <iqbalwali786@gmail.com>
2026-09-02 09:19:43 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,29 @@ import { Playfair_Display, Inter } from "next/font/google";
 import WhatsAppButton from "./components/WhatsappButton";
 
 export const metadata = {
-  title: "Khatamband Kashmir",
+  title: "Khatamband Ceiling in Kashmir",
   description: "Preserving the sacred Kashmiri Khatamband ceiling heritage",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+
+  manifest: "/site.webmanifest",
 };
 
 const playfair = Playfair_Display({
